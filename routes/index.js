fccbfc9a8a8066f2f42ae600c675bd27d391415d
Gabriele Sammartino <Gabriele Sammartino>
2017-03-22 23:56:24 +0100

    1.2

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 var cron = require('cron');
 var request = require('request');
+global.a = 0;
 
-var cronJob = cron.job("0 */10 * * * *", function(){
+
+var cronJob = cron.job('* * * * * *', function(){
   console.log("inizio");
     request.get('http://localhost:3000/', function (error, response, body) {
 
     });
-    router.get('*', function(req, res) {
+    router.get('/', function(req, res) {
         res.redirect('http://www.weathersicily.it/Servizi/WeatherSicily.html')
+        a++;
+        res.send(a);
     });
 
     console.info('cron job completed');
